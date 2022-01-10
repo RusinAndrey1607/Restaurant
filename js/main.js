48429e52735e872bc5b9d89ed0ca17351b4edac7
RusinAndrey1607 <rusinandrey1607@gmail.com>
2022-01-10 12:16:03 +0300

    adaptive

--- js/main.js
+++ js/main.js
@@ -13,12 +13,29 @@ const two = document.querySelector('.number--two')
 const three = document.querySelector('.number--three')
 
 
-const intervalId = setInterval(() => {
-    one__number < 93 && one__number++;
-    two__number < 206 && two__number++;
-    three__number < 71 && three__number++;
-    one.innerHTML = one__number
-    two.innerHTML = two__number
-    three.innerHTML = three__number
-    two__number == 206 && clearInterval(intervalId)
-}, 100)
+// const intervalId = setInterval(() => {
+//     one__number < 93 && one__number++;
+//     two__number < 206 && two__number++;
+//     three__number < 71 && three__number++;
+//     one.innerHTML = one__number
+//     two.innerHTML = two__number
+//     three.innerHTML = three__number
+//     two__number == 206 && clearInterval(intervalId)
+// }, 100)
+
+const burger = document.querySelector(".header__burger");
+const menu = document.querySelector(".header__menu");
+const body = document.querySelector(".body");
+
+burger.addEventListener("click", () => {
+    burger.classList.toggle("active");
+    menu.classList.toggle("active");
+    body.classList.toggle("active");
+});
+
+
+menu.addEventListener("click", () => {
+    burger.classList.remove("active");
+    menu.classList.remove("active");
+    body.classList.remove("active");
+})
